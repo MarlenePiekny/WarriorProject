1cb47dbe2d13c1df04f1762a225bc84c1ec2b81c
MarlenePiekny <marlene.piekny@gmail.com>
2021-04-28 16:20:05 +0200

    feat: Welcome to Warrior game display message #12

---
+++ src/main/java/warrior/Menu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Menu {
+
+public static void main(String[] args) {
+
+	Scanner keyboard = new Scanner(System.in);
+	
+	System.out.println("Welcome in the Warrior game");
+}
+
+}
